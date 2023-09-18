75b12acfecd196cebef2bc11fd1e9f2b49e44209
Veliti <71709424+Veliti@users.noreply.github.com>
2023-09-18 20:54:12 +0500

    sm

--- CSSClasses/lsp/jrcp.lua
+++ CSSClasses/lsp/jrcp.lua
@@ -2,25 +2,66 @@ local json = require("deps.json")
 
 local M = {}
 
-M.encode = function(method, params)
+local pack = function(body)
+	local content = json.encode(body)
+	return table.concat({
+		"Content-Length: ",
+		#content,
+		"\r\n\r\n",
+		content,
+	})
+end
+
+M.notify = function(method, params)
 	local body = {
 		["jsonrpc"] = "2,0",
-		["id"] = 1,
 		["method"] = method,
 		["params"] = params,
 	}
-	local content = json.encode(body)
-	return "Content-Length: " .. #content .. "\r\n\r\n" .. content
+	return pack(body)
 end
 
+M.response = function(id, method, response)
+	local body = {
+		["jsonrpc"] = "2,0",
+		["id"] = id,
+		["method"] = method,
+		["response"] = response,
+	}
+	return pack(body)
+end
+
+M.error = function(err_code, message)
+	local body = {
+		["code"] = err_code,
+		["message"] = message,
+	}
+	return pack(body)
+end
+
+---try to decode message
 ---@param data string
 M.decode = function(data)
-	local s, e, length = data:find("Content-Length:([%d ]+)\r\n")
-	length = assert(tonumber(length), "RPC: COULDNT READ LENGTH")
-	local s, e = data:find(".*\r\n", e)
+	--- TODO: could be optimised luvit/json capable of part-parsing
+	local _, fend, length = data:find("Content-Length: ([%d]+)\r\n")
+	if not fend then
+		return
+	end
+	local _, fend = data:find("\r\n", fend)
+	if not fend then
+		return
+	end
 	-- check if we have full message
-	if #data - e >= length then
-		local response = json.decode(data:sub(e, e + length))
+	if #data - fend >= length then
+		local message = json.decode(data:sub(fend, fend + length))
+		if not message then
+			local err = "RPC: Message decode error"
+			M.error(M.ERROR_CODES.ParseError, err)
+			error(err)
+		else
+			local rest = data:sub(fend + length, #data)
+			return message, rest
+		end
 	end
 end
 
---
+++ CSSClasses/tests/protocol.lua
@@ -0,0 +1,16 @@
+local rcp = require("CSSClasses.lsp.jrcp")
+
+rcp_test = function()
+	local method = "test/testing"
+	local params = {
+		hello = "world",
+		number = 3,
+	}
+	local encoded = rcp.notify(method, params)
+	local decoded = rcp.decode(encoded)
+
+	assert(decoded.method == method)
+	assert(decoded.params.hello == params.hello)
+	assert(decoded.params.number == params.number)
+end
+rcp_test()
